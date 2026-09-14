9c4c8ef8eacd33ca4fbe4f3d6370a6aebbc9096c
Chioma750 <ugwuchioma778@gmail.com>
2026-09-14 02:08:35 +0100

    games

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import random
 secret_number = random.randint(1, 100)
 attempt = 0
+max_attempt = 5
 
-while True:
+while attempt < max_attempt:
     guess = int(input("guess a number from 1 to 100: "))
     attempt += 1
     
-    if guess > secret_number:
+    if guess == secret_number:
+        print("You are correct ☺️")
+        print("you attempted", attempt, "times")
+        break 
+    elif attempt == max_attempt:
+        print("You Failed 🤣")
+        print("The answer is", secret_number)
+    elif guess > secret_number:
         print("Too high")
     elif guess < secret_number:
         print("Too low")
-    elif guess == secret_number:
-        print("You are correct")
-        print("you attempted", attempt, "times")
-        break 
-    else:
-        continue
  
